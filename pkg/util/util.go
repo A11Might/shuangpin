88c957121dfac2fd4b47619141002c658f031060
huqihang <huqihang@bytedance.com>
2021-09-11 18:21:30 +0800

    test for dongdong

---
+++ pkg/util/util.go
@@ -0,0 +1,38 @@
+package util
+
+import "strings"
+
+// conversion [["zhong"] ["guo"] ["ren"]] to ["zhong", "guo", "ren"]
+func conversion(twoD [][]string) []string {
+	result := make([]string, 0)
+	for _, item := range twoD {
+		result = append(result, item[0])
+	}
+	return result
+}
+
+// symbol 判断是否是符号
+func symbol(str string) bool {
+	if str == "," || str == "." {
+		return true
+	}
+	return false
+}
+
+// handlingText ，/。 to ,/. 带符号的句子还在开发
+func handlingText(text string) string {
+	text = strings.Replace(text, " ", "", -1)
+	text = strings.Replace(text, "\n", "", -1)
+	text = strings.Replace(text, "，", "", -1)
+	text = strings.Replace(text, "。", "", -1)
+	text = strings.Replace(text, ",", "", -1)
+	text = strings.Replace(text, ".", "", -1)
+	text = strings.Replace(text, "·", "", -1)
+	text = strings.Replace(text, ":", "", -1)
+	text = strings.Replace(text, "：", "", -1)
+	return text
+}
+
+func GetShuangpin(char string) []string {
+	return getNaturalCodeKeyToPinyin()[char]
+}
